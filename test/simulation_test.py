c0e9bb49ece27525d3ebfb2de55160e98737039f
kunxl-gg <tiwari.25@iitj.ac.in>
2025-01-15 22:31:35 +0530

    feat: add boiler plate for tests
    Signed-off-by: kunxl-gg <tiwari.25@iitj.ac.in>

---
+++ test/simulation_test.py
@@ -0,0 +1,7 @@
+import unittest
+
+class TestSimulation(unittest.TestCase):
+	pass
+
+if __name__ == "__main__":
+	unittest.main()
---
+++ test/strategy_test.py
@@ -0,0 +1,7 @@
+import unittest
+
+class TestStrategy(unittest.TestCase):
+	pass
+
+if __name__ == "__main__":
+	unittest.main()
