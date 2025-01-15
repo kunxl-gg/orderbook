1a5855ed4ca0166c925fad35363df0c097fe66aa
kunxl-gg <tiwari.25@iitj.ac.in>
2025-01-15 12:11:04 +0530

    feat: add read/write features to the logger class
    Signed-off-by: kunxl-gg <tiwari.25@iitj.ac.in>

--- logger.py
+++ logger.py
@@ -1,7 +1,43 @@
-class Logger:
-	def __init__(self):
-		self.file = "transactions.log"
-		
+import os
+import json
+from datetime import datetime
 
-	def log(self):
-		pass
+class Logger:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+        if not os.path.exists(self.file_path):
+            with open(self.file_path, 'w') as f:
+                json.dump([], f)
+
+    def _load_transactions(self):
+        with open(self.file_path, 'r') as f:
+            return json.load(f)
+
+    def _save_transactions(self, transactions):
+        with open(self.file_path, 'w') as f:
+            json.dump(transactions, f, indent=4)
+
+    def record_transaction(self, transaction_id, expiry):
+        transactions = self._load_transactions()
+
+        transactions.append({"id": transaction_id, "expiry": expiry})
+
+        transactions.sort(key=lambda x: datetime.fromisoformat(x['expiry']))
+
+        self._save_transactions(transactions)
+
+    def first_transaction(self):
+        transactions = self._load_transactions()
+
+        if transactions:
+            return transactions[0]
+        else:
+            print("No transactions available.")
+
+    def read_transactions(self):
+        return self._load_transactions()
+
+    def clear_transactions(self):
+        self._save_transactions([])
+
