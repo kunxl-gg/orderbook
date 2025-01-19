e5a234bb340c16bb6ccaa39328cf4aae5df889df
kunxl-gg <tiwari.25@iitj.ac.in>
2025-01-19 19:13:04 +0530

    fix: fix code-formatting in logger.py
    Signed-off-by: kunxl-gg <tiwari.25@iitj.ac.in>

--- logger.py
+++ logger.py
@@ -1,26 +1,34 @@
 import csv
 import datetime as dt
+from dataclasses import dataclass
+
+@dataclass
+class LogDetails:
+    id: int
+    kind: str
+    date: dt.date
+    expiry = dt.date
+    strike_price: float
 
 class Logger:
-	def __init__(self, file_path, log_file):
-		self.file_path = file_path
-		self.log_file = log_file
+    def __init__(self, file_path, log_file):
+        self.file_path = file_path
+        self.log_file = log_file
 
-		with open(self.file_path, 'w', newline='') as f:
-			writer = csv.writer(f)
-			writer.writerow(["id", "date", "type", "strike_price", "expiry"])
+        with open(self.file_path, "w", newline="", encoding="UTF-8") as f:
+            writer = csv.writer(f)
+            writer.writerow(["id", "date", "type", "strike_price", "expiry"])
 
-		with open(self.log_file, 'w', newline='') as f:
-			writer = csv.writer(f)
-			writer.writerow(["capital", "date"])
+        with open(self.log_file, 'w', newline="", encoding="UTF-8") as f:
+            writer = csv.writer(f)
+            writer.writerow(["capital", "date"])
 
-	def record_transaction(self, transaction_id, date, type, strike_price, expiry):
-		with open(self.file_path, 'a', newline='') as f:
-			writer = csv.writer(f)
-			writer.writerow([transaction_id, date, type, strike_price, expiry])
+    def record_transaction(self, log: LogDetails):
+        with open(self.file_path, "a", newline="", encoding="UTF-8") as f:
+            writer = csv.writer(f)
+            writer.writerow([log.id, log.kind, log.date, log.expiry, log.strike_price])
 
-
-	def record_revenue(self, revenue, date):
-		with open(self.log_file, 'a', newline='') as f:
-			writer = csv.writer(f)
-			writer.writerow([revenue, date])
+    def record_revenue(self, revenue, date):
+        with open(self.log_file, "a", newline="", encoding="UTF-8") as f:
+            writer = csv.writer(f)
+            writer.writerow([revenue, date])
