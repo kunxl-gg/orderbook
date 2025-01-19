e04f8e209f0cffef3b937d2dabc79250cbafbc66
kunxl-gg <tiwari.25@iitj.ac.in>
2025-01-19 19:28:43 +0530

    fix: fix indentation in strategy.py
    Signed-off-by: kunxl-gg <tiwari.25@iitj.ac.in>

--- strategy.py
+++ strategy.py
@@ -2,57 +2,57 @@ import datetime as dt
 import uuid
 
 class Strategy:
-	def __init__(self, simulator, premium, margin):
-		self.simulator = simulator
-		self.premium = premium
-		self.margin = margin
+    def __init__(self, simulator, premium, margin):
+        self.simulator = simulator
+        self.premium = premium
+        self.margin = margin
 
-	def get_premium(self, type):
-		if type == "long call":
-			return float(2.5 * self.premium * self.simulator.get_price())
-		else:
-			return float(self.premium * self.simulator.get_price())
+    def get_premium(self, type):
+        if type == "long call":
+            return float(2.5 * self.premium * self.simulator.get_price())
+        else:
+            return float(self.premium * self.simulator.get_price())
 
 
-	def is_expired(self, transaction):
-		return self.simulator.today >= transaction["expiry"]
+    def is_expired(self, transaction):
+        return self.simulator.today >= transaction["expiry"]
 
-	def should_buy(self):
-		today = self.simulator.today
-		last_transaction = self.simulator.last_transaction
+    def should_buy(self):
+        today = self.simulator.today
+        last_transaction = self.simulator.last_transaction
 
-		if last_transaction == -1:
-			return True
+        if last_transaction == -1:
+            return True
 
-		current_week = self.simulator.today.isocalendar().week
-		last_week = last_transaction.isocalendar().week
+        current_week = self.simulator.today.isocalendar().week
+        last_week = last_transaction.isocalendar().week
 
-		if today.isoformat() > (self.simulator.end - dt.timedelta(days=28)).isoformat():
-			return False
+        if today.isoformat() > (self.simulator.end - dt.timedelta(days=28)).isoformat():
+            return False
 
-		if current_week == last_week:
-			return False
-		if today.isoformat() < last_transaction.isoformat():
-			return False
+        if current_week == last_week:
+            return False
+        if today.isoformat() < last_transaction.isoformat():
+            return False
 
-		return True
+        return True
 
-	def run(self):
-		# Check for expired options
-		for transaction in self.simulator.active_transactions:
-			if self.simulator.today >= transaction["expiry"]:
-				self.simulator.exit(transaction["type"], transaction["id"])
+    def run(self):
+        # Check for expired options
+        for transaction in self.simulator.active_transactions:
+            if self.simulator.today >= transaction["expiry"]:
+                self.simulator.exit(transaction["type"], transaction["id"])
 
-		# Find out the next expiry
-		days_ahead = (3 - self.simulator.today.weekday() + 7) % 7
-		days_ahead = days_ahead or 7
+        # Find out the next expiry
+        days_ahead = (3 - self.simulator.today.weekday() + 7) % 7
+        days_ahead = days_ahead or 7
 
-		if self.should_buy():
-			expiry = self.simulator.today + dt.timedelta(days=days_ahead)
-			self.simulator.buy("short call", expiry, 10)
+        if self.should_buy():
+            expiry = self.simulator.today + dt.timedelta(days=days_ahead)
+            self.simulator.buy("short call", expiry, 10)
 
-		if not self.simulator.long_call:
-			days_ahead = days_ahead + 3 * 7
-			expiry = self.simulator.today + dt.timedelta(days=days_ahead)
+        if not self.simulator.long_call:
+            days_ahead = days_ahead + 3 * 7
+            expiry = self.simulator.today + dt.timedelta(days=days_ahead)
 
-			self.simulator.buy("long call", expiry, 10)
+            self.simulator.buy("long call", expiry, 10)
